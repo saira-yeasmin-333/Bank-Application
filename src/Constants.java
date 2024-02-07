113dc7521098e89f2bbf7cb1728b70ae4055af31
saira-yeasmin-333 <sairayeasminsa@gmail.com>
2024-02-07 15:00:58 +0600

    sub accounts are created and base class done

---
+++ src/Constants.java
@@ -0,0 +1,10 @@
+public class Constants {
+    public static final String SAVING_ACCOUNT="Savings account";
+    public static final String CURRENT_ACCOUNT="Current account";
+    public static final double  SAVING_OPENINGS=500;
+    public static final double  CURRENT_OPENINGS=200;
+
+    public static final double  SAVING_MINIMUM=500;
+    public static final double  CURRENT_MINIMUM=200;
+
+}
